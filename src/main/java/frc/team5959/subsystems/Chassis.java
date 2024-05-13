eb7c422228a5f30f46eb3bf6505456942d00465f
ElProfeAlviso <alejandro.alviso@tec.mx>
2024-05-13 13:29:36 -0600

    Prueba comentario Git

--- src/main/java/frc/team5959/subsystems/Chassis.java
+++ src/main/java/frc/team5959/subsystems/Chassis.java
@@ -20,7 +20,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.kauailabs.navx.frc.*;
 import com.pathplanner.lib.auto.AutoBuilder;
 
-//prueba
+//prueba/////
 
 public class Chassis extends SubsystemBase{
 
